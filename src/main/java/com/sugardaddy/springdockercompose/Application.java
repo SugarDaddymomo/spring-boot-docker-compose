6b2aad01abb8a4dfb2e01857136bbcdc318a9d89
SugarDaddy <sugarmomo@sd1876.com>
2023-04-27 12:01:29 +0530

    Added a record through CommandLineRunner bean.

--- src/main/java/com/sugardaddy/springdockercompose/Application.java
+++ src/main/java/com/sugardaddy/springdockercompose/Application.java
@@ -1,13 +1,22 @@
 package com.sugardaddy.springdockercompose;
 
+import com.sugardaddy.springdockercompose.model.Post;
+import com.sugardaddy.springdockercompose.repo.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(PostRepository repository) {
+		return args -> repository.save(new Post("Hello, World!", "My first blog post"));
 	}
 
 }
